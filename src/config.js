bfea290920384a873b8fb1ee9a90b164822d0429
kvvarun-mecha <varunkkv.mec@gmail.com>
2025-03-03 01:40:46 +0530

    init

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export const metaData = {
+  baseUrl: "https://vrn21.vercel.app/",
+  title: "vrn21",
+  name: "Varun",
+  ogImage: "/opengraph-image.png",
+  description:
+    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com/theVRN21",
+  github: "https://github.com/vrn21",
+  instagram: "https://www.instagram.com/vrn21.x",
+  linkedin: "https://www.linkedin.com/in/hi-from-varun-",
+  email: "mailto:varunkkv.mec@gmail.com",
+};
